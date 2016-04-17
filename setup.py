4a00097eb43f96c2d0881f78854132eaa38fab9b
Muhammed Uluyol <uluyol0@gmail.com>
2016-04-16 20:45:34 -0400

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.rst") as f:
-	readme = f.read()
-
-with open("LICENSE") as f:
-	license = f.read()
-
 setup(
 	name="gdo",
-	version="0.1.0",
+	version="0.1.1",
 	description="Concurrent execution library",
-	long_description=readme,
+	long_description="",
 	author="Muhammed Uluyol",
 	author_email="uluyol0@gmail.com",
 	url="https://github.com/uluyol/gdo",
-	license=license,
+	license="MIT",
 	packages=find_packages(exclude=("tests", "docs")),
 	test_suite="tests",
 )
